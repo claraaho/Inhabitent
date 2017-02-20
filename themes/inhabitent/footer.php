1487f848dec76921c38b79cd1c789559ef21bd64
Clara <claraho@live.ca>
2017-02-19 20:14:35 -0800

    Applied basic styling

--- themes/inhabitent/footer.php
+++ themes/inhabitent/footer.php
@@ -11,12 +11,30 @@
 
 			<footer id="colophon" class="site-footer" role="contentinfo">
 				<div class="site-info">
-					<a href="<?php echo esc_url( 'https://wordpress.org/' ); ?>"><?php printf( esc_html( 'Proudly powered by %s' ), 'WordPress' ); ?></a>
+					<div class="contact-info">
+						<h2>Contact Info</h2>
+						<p><a href=""><i class="fa fa-envelope" aria-hidden="true"></i></i>info@inhabitent.com</a></p>
+						<p><a href=""><i class="fa fa-phone fa-lg" aria-hidden="true"></i>778-456-7891</a></p>
+						<div class="icons">
+							<i class="fa fa-facebook-square fa-lg" aria-hidden="true"></i>
+							<i class="fa fa-twitter-square fa-lg" aria-hidden="true"></i>
+							<i class="fa fa-google-plus-square fa-lg" aria-hidden="true"></i>
+						</div>
+					</div>
+					<div class="business-hours">
+						<h2>Business Hours</h2>
+						<p>Monday-Friday: 9am to 5pm</p>
+						<p>Saturday: 10am to 2pm</p>
+						<p>Sunday: Closed</p>
+					</div>
+					<img src="./images/inhabitent-logo-text.svg" alt="Inhabitent logo">
+					<!--<a href="<?php echo esc_url( 'https://wordpress.org/' ); ?>"><?php printf( esc_html( 'Proudly powered by %s' ), 'WordPress' ); ?></a>-->
 				</div><!-- .site-info -->
+				<p class="copyright">Copyright &copy; 2016 Inhabitent</p>
 			</footer><!-- #colophon -->
 		</div><!-- #page -->
 
 		<?php wp_footer(); ?>
-
+		
 	</body>
 </html>
--- themes/inhabitent/header.php
+++ themes/inhabitent/header.php
@@ -12,7 +12,7 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<link rel="profile" href="http://gmpg.org/xfn/11">
 		<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
-
+		<script src="https://use.fontawesome.com/0495204cae.js"></script>
 	<?php wp_head(); ?>
 	</head>
 
@@ -21,7 +21,9 @@
 			<a class="skip-link screen-reader-text" href="#content"><?php esc_html( 'Skip to content' ); ?></a>
 
 			<header id="masthead" class="site-header" role="banner">
+				<div class="header-container">
 				<div class="site-branding">
+					<div class="logo"></div>
 					<h1 class="site-title screen-reader-text"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
 					<p class="site-description"><?php bloginfo( 'description' ); ?></p>
 				</div><!-- .site-branding -->
@@ -30,6 +32,7 @@
 					<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html( 'Primary Menu' ); ?></button>
 					<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu' ) ); ?>
 				</nav><!-- #site-navigation -->
+				</div><!-- .header-container -->
 			</header><!-- #masthead -->
 
 			<div id="content" class="site-content">
