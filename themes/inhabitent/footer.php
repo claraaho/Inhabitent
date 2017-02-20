81778aa5f4ddf548f6cfe8ec601a62ef773db2f9
Clara <claraho@live.ca>
2017-02-19 22:04:22 -0800

    Styled some more

--- themes/inhabitent/footer.php
+++ themes/inhabitent/footer.php
@@ -11,24 +11,25 @@
 
 			<footer id="colophon" class="site-footer" role="contentinfo">
 				<div class="site-info">
+					<div class="info-wrapper">
 					<div class="contact-info">
-						<h2>Contact Info</h2>
+						<h3>Contact Info</h3>
 						<p><a href=""><i class="fa fa-envelope" aria-hidden="true"></i></i>info@inhabitent.com</a></p>
 						<p><a href=""><i class="fa fa-phone fa-lg" aria-hidden="true"></i>778-456-7891</a></p>
-						<div class="icons">
+						<p>
 							<i class="fa fa-facebook-square fa-lg" aria-hidden="true"></i>
 							<i class="fa fa-twitter-square fa-lg" aria-hidden="true"></i>
 							<i class="fa fa-google-plus-square fa-lg" aria-hidden="true"></i>
-						</div>
+						</p>
 					</div>
 					<div class="business-hours">
-						<h2>Business Hours</h2>
+						<h3>Business Hours</h3>
 						<p>Monday-Friday: 9am to 5pm</p>
 						<p>Saturday: 10am to 2pm</p>
 						<p>Sunday: Closed</p>
 					</div>
-					<img src="./images/inhabitent-logo-text.svg" alt="Inhabitent logo">
-					<!--<a href="<?php echo esc_url( 'https://wordpress.org/' ); ?>"><?php printf( esc_html( 'Proudly powered by %s' ), 'WordPress' ); ?></a>-->
+					</div>
+					<img src="<?php echo get_template_directory_uri() ?>/images/inhabitent-logo-text.svg" alt="Inhabitent Logo">
 				</div><!-- .site-info -->
 				<p class="copyright">Copyright &copy; 2016 Inhabitent</p>
 			</footer><!-- #colophon -->
