bd67829649ab8bf47292ed79bd06e5d9b7bea50d
Clara <clara@PIKACHU-2.local>
2017-03-02 15:33:34 -0800

    edited indenting

--- themes/inhabitent/functions.php
+++ themes/inhabitent/functions.php
@@ -65,24 +65,24 @@ function red_starter_widgets_init() {
 		'after_title'   => '</h2>',
 	) );
 	register_sidebar( array(
-'name' => 'Footer Sidebar 1',
-'id' => 'footer-sidebar-1',
-'description' => 'Appears in the footer area',
-'before_widget' => '<aside id="%1$s" class="widget %2$s">',
-'after_widget' => '</aside>',
-'before_title' => '<h3 class="widget-title">',
-'after_title' => '</h3>',
-) );
-register_sidebar( array(
-'name' => 'Footer Sidebar 2',
-'id' => 'footer-sidebar-2',
-'description' => 'Appears in the footer area',
-'before_widget' => '<aside id="%1$s" class="widget %2$s">',
-'after_widget' => '</aside>',
-'before_title' => '<h3 class="widget-title">',
-'after_title' => '</h3>',
-) );
-}
+		'name' => 'Footer Sidebar 1',
+		'id' => 'footer-sidebar-1',
+		'description' => 'Appears in the footer area',
+		'before_widget' => '<aside id="%1$s" class="widget %2$s">',
+		'after_widget' => '</aside>',
+		'before_title' => '<h3 class="widget-title">',
+		'after_title' => '</h3>',
+	) );
+	register_sidebar( array(
+		'name' => 'Footer Sidebar 2',
+		'id' => 'footer-sidebar-2',
+		'description' => 'Appears in the footer area',
+		'before_widget' => '<aside id="%1$s" class="widget %2$s">',
+		'after_widget' => '</aside>',
+		'before_title' => '<h3 class="widget-title">',
+		'after_title' => '</h3>',
+	) );
+}	
 add_action( 'widgets_init', 'red_starter_widgets_init' );
 
 /**
